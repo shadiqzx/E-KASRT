cb432bc20ec7752a093d43ff2e4d5df0fb0ce2b7
Syaifullah Asshadiq <blackshadiqo@gmail.com>
2026-07-01 19:15:35 -0700

    Auto backup 2026-07-02 04:15:35

--- application/controllers/KasRT.php
+++ application/controllers/KasRT.php
@@ -23,34 +23,24 @@ class KasRT extends CI_Controller
 		if ($username == '') {
 			redirect('auth');
 		} else {
-			if ($user['role_id'] == 1) {
-				$data['menu'] = 'Kas Masuk';
-				$data['judul'] = 'Kas Masuk';
-				$data['user'] = $user;
-				$data['ttl'] = $this->m_kas->TotalMasuk();
-				$data['masuk'] = $this->m_kas->getKasMasuk();
-				$this->load->view('include/header', $data);
-				$this->load->view('admin/kasMasuk', $data);
-				$this->load->view('include/footer');
-			} else if ($user['role_id'] == 3) {
-				$data['menu'] = 'Kas Masuk';
-				$data['judul'] = 'Kas Masuk';
-				$data['user'] = $user;
-				$data['ttl'] = $this->m_kas->TotalMasuk();
-				$data['masuk'] = $this->m_kas->getKasMasuk();
-				$this->load->view('include/header_1', $data);
-				$this->load->view('bendahara/kasMasuk', $data);
-				$this->load->view('include/footer');
-			} else {
-				$data['menu'] = 'Kas Masuk';
-				$data['judul'] = 'Kas Masuk';
-				$data['user'] = $user;
-				$data['ttl'] = $this->m_kas->TotalMasuk();
-				$data['masuk'] = $this->m_kas->getKasMasuk();
-				$this->load->view('include/header_1', $data);
-				$this->load->view('rt/kasMasuk', $data);
-				$this->load->view('include/footer');
+			if ($user['role_id'] == 4) {
+				redirect('users/warga');
 			}
+			$data['user'] = $user;
+			$data['ttl'] = $this->m_kas->TotalMasuk();
+			$data['masuk'] = $this->m_kas->getKasMasuk();
+			$data['menu'] = 'Kas Masuk';
+			$data['judul'] = 'Kas Masuk';
+
+			$this->load->view('include/header', $data);
+			if ($user['role_id'] == 1) {
+				$this->load->view('admin/kasMasuk', $data);
+			} else if ($user['role_id'] == 3) {
+				$this->load->view('bendahara/kasMasuk', $data);
+			} else {
+				$this->load->view('rt/kasMasuk', $data);
+			}
+			$this->load->view('include/footer');
 		}
 	}
 
@@ -67,39 +57,35 @@ class KasRT extends CI_Controller
 		if ($username == '') {
 			redirect('auth');
 		} else {
-			if ($user['role_id'] == 1) {
-				$data['menu'] = 'Kas Keluar';
-				$data['judul'] = 'Kas Keluar';
-				$data['user'] = $user;
-				$data['ttl'] = $this->m_kas->TotalKeluar();
-				$data['keluar'] = $this->m_kas->getKasKeluar();
-				$this->load->view('include/header', $data);
-				$this->load->view('admin/kasKeluar', $data);
-				$this->load->view('include/footer');
-			} else if ($user['role_id'] == 3) {
-				$data['menu'] = 'Kas Keluar';
-				$data['judul'] = 'Kas Keluar';
-				$data['user'] = $user;
-				$data['ttl'] = $this->m_kas->TotalKeluar();
-				$data['keluar'] = $this->m_kas->getKasKeluar();
-				$this->load->view('include/header_1', $data);
-				$this->load->view('bendahara/kasKeluar', $data);
-				$this->load->view('include/footer');
-			} else {
-				$data['menu'] = 'Kas Keluar';
-				$data['judul'] = 'Kas Keluar';
-				$data['user'] = $user;
-				$data['ttl'] = $this->m_kas->TotalKeluar();
-				$data['keluar'] = $this->m_kas->getKasKeluar();
-				$this->load->view('include/header_1', $data);
-				$this->load->view('rt/kasKeluar', $data);
-				$this->load->view('include/footer');
+			if ($user['role_id'] == 4) {
+				redirect('users/warga');
 			}
+			$data['user'] = $user;
+			$data['ttl'] = $this->m_kas->TotalKeluar();
+			$data['keluar'] = $this->m_kas->getKasKeluar();
+			$data['menu'] = 'Kas Keluar';
+			$data['judul'] = 'Kas Keluar';
+
+			$this->load->view('include/header', $data);
+			if ($user['role_id'] == 1) {
+				$this->load->view('admin/kasKeluar', $data);
+			} else if ($user['role_id'] == 3) {
+				$this->load->view('bendahara/kasKeluar', $data);
+			} else {
+				$this->load->view('rt/kasKeluar', $data);
+			}
+			$this->load->view('include/footer');
 		}
 	}
 
 	public function addKas()
 	{
+		$username = $this->session->userdata('username');
+		$user = $this->db->get_where('users', ['username' => $username])->row_array();
+		if (!$user || $user['role_id'] == 4) {
+			redirect('users/warga');
+		}
+
 		$this->m_kas->cekNomor();
 		$data = [
 			'idKas' => $this->input->post('id_kas'),
@@ -109,7 +95,7 @@ class KasRT extends CI_Controller
 			'jenis' => $this->input->post('jenis'),
 		];
 		$this->m_kas->saveKas($data);
-		if ('jenis' == 'masuk') {
+		if ($this->input->post('jenis') == 'masuk') {
 			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data berhasil ditambahkan!</div>');
 			redirect('kasrt');
 		} else {
@@ -120,6 +106,12 @@ class KasRT extends CI_Controller
 
 	public function editKas()
 	{
+		$username = $this->session->userdata('username');
+		$user = $this->db->get_where('users', ['username' => $username])->row_array();
+		if (!$user || $user['role_id'] == 4) {
+			redirect('users/warga');
+		}
+
 		$idKas = $this->input->post('idKas');
 		$data = [
 			'keterangan' => $this->input->post('keterangan'),
@@ -128,7 +120,7 @@ class KasRT extends CI_Controller
 			'jenis' => $this->input->post('jenis'),
 		];
 		$this->m_kas->updateKas($data, $idKas);
-		if ('jenis' == 'masuk') {
+		if ($this->input->post('jenis') == 'masuk') {
 			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data berhasil diupdate!</div>');
 			redirect('kasrt');
 		} else {
@@ -139,14 +131,15 @@ class KasRT extends CI_Controller
 
 	public function delKas($idKas)
 	{
-		$this->m_kas->delKas($idKas);
-		if ('jenis' == 'masuk') {
-			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data berhasil dihapus!</div>');
-			redirect('kasrt');
-		} else {
-			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data berhasil dihapus!</div>');
-			redirect('kasrt/kasKeluar');
+		$username = $this->session->userdata('username');
+		$user = $this->db->get_where('users', ['username' => $username])->row_array();
+		if (!$user || $user['role_id'] == 4) {
+			redirect('users/warga');
 		}
+
+		$this->m_kas->delKas($idKas);
+		$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data berhasil dihapus!</div>');
+		redirect('kasrt');
 	}
 
 	public function laporan()
@@ -157,56 +150,36 @@ class KasRT extends CI_Controller
 		if ($username == '') {
 			redirect('auth');
 		} else {
-			if ($user['role_id'] == 1) {
-				$data['menu'] = 'Laporan';
-				$data['judul'] = 'Laporan Kas RT';
-				$data['user'] = $user;
+			$data['menu'] = 'Laporan';
+			$data['judul'] = 'Laporan Kas RT';
+			$data['user'] = $user;
+			$data['kas'] = $this->m_kas->getKas();
+			$data['masuk'] = $this->m_kas->TotalMasuk();
+			$data['keluar'] = $this->m_kas->TotalKeluar();
+
+			if ($user['role_id'] == 1 || $user['role_id'] == 2 || $user['role_id'] == 3) {
 				$data['debit'] = $this->m_kas->getKasMasuk();
 				$data['kredit'] = $this->m_kas->getKasKeluar();
-				$data['kas'] = $this->m_kas->getKas();
-				$data['masuk'] = $this->m_kas->TotalMasuk();
-				$data['keluar'] = $this->m_kas->TotalKeluar();
 				$this->load->view('include/header', $data);
-				$this->load->view('admin/laporan', $data);
-				$this->load->view('include/footer');
-			} else if ($user['role_id'] == 3) {
-				$data['menu'] = 'Laporan';
-				$data['judul'] = 'Laporan Kas RT';
-				$data['user'] = $user;
-				$data['kas'] = $this->m_kas->getKas();
-				$data['masuk'] = $this->m_kas->TotalMasuk();
-				$data['keluar'] = $this->m_kas->TotalKeluar();
-				$this->load->view('include/header_1', $data);
-				$this->load->view('bendahara/laporan', $data);
-				$this->load->view('include/footer');
-			} else if ($user['role_id'] == 2) {
-				$data['menu'] = 'Laporan';
-				$data['judul'] = 'Laporan Kas RT';
-				$data['user'] = $user;
-				$data['kas'] = $this->m_kas->getKas();
-				$data['masuk'] = $this->m_kas->TotalMasuk();
-				$data['keluar'] = $this->m_kas->TotalKeluar();
-				$this->load->view('include/header_1', $data);
-				$this->load->view('rt/laporan', $data);
-				$this->load->view('include/footer');
+				if ($user['role_id'] == 1) {
+					$this->load->view('admin/laporan', $data);
+				} else if ($user['role_id'] == 3) {
+					$this->load->view('bendahara/laporan', $data);
+				} else {
+					$this->load->view('rt/laporan', $data);
+				}
 			} else {
-				$data['menu'] = 'Laporan';
-				$data['judul'] = 'Laporan Kas RT';
-				$data['user'] = $user;
-				$data['kas'] = $this->m_kas->getKas();
-				$data['masuk'] = $this->m_kas->TotalMasuk();
-				$data['keluar'] = $this->m_kas->TotalKeluar();
 				$this->load->view('include/header_warga', $data);
 				$this->load->view('warga/laporan', $data);
-				$this->load->view('include/footer');
 			}
+			$this->load->view('include/footer');
 		}
 	}
 
 	public function lapKas() {
         $data['judul'] = 'Laporan Data Kas RT002';
         $data['kas'] = $this->M_kas->getKas();
-        $data['kredit'] = $this->M_kas->kredit(); // Memanggil method kredit dari model
+        $data['kredit'] = $this->M_kas->kredit(); 
         $data['masuk'] = $this->M_kas->TotalMasuk();
         $data['keluar'] = $this->M_kas->TotalKeluar();
         $data['konten'] = 'lap_kas';
